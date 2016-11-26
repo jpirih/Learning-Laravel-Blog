8c8b614e7799136222d8c1bade2221b3a7e2ef79
Janko Pirih <janko.pirih@gmail.com>
2016-11-26 17:17:58 +0100

    google login test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,15 +24,16 @@
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => [['web']], function () {
     Route::get('/', ['uses' => 'SiteController@index']);
     Route::get('/blog', ['middleware'=> 'auth','uses' => 'PostController@posts', 'as' => 'blog']);
     Route::get('o-meni', ['uses' => 'SiteController@about']);
     Route::get('kontakt',['uses' => 'SiteController@contact']);
-    Route::get('nova-objava', ['middleware' => 'auth', 'uses' => 'PostController@writeNewPost', 'as' => 'new_post']);
+    Route::get('nova-objava', ['uses' => 'PostController@writeNewPost', 'as' => 'new_post']);
     Route::post('nova-objava', ['uses' => 'PostController@saveNewPost']);
     Route::get('objava/{id}', ['uses' => 'PostController@postDetails', 'as' => 'details']);
-    Route::get('zbirnik-objav', ['uses' => 'PostController@allPosts', 'as' => 'dashboard']);
+    Route::get('zbirnik-objav', ['middleware' => 'auth','uses' => 'PostController@allPosts', 'as' => 'dashboard']);
+    Route::get('zbirnik-objav', ['middleware' => 'auth','uses' => 'PostController@allPosts', 'as' => 'dashboard']);
     Route::post('create-category', ['uses' => 'PostController@saveCategory', 'as' => 'crete_category']);
     Route::post('objava/{id}/comment', ['uses' => 'PostController@saveComment', 'as' => 'save_comment']);
     Route::get('objava/{id}/update', ['uses' => 'PostController@editPost', 'as' => 'edit_post']);
