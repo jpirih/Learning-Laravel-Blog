468630d7fc1f895676d8d4d44500f9f7d5954c94
Janko Pirih <janko.pirih@gmail.com>
2016-12-05 18:33:23 +0100

    updated post commenting -> cleaning a little

--- resources/views/pages/user_profile.blade.php
+++ resources/views/pages/user_profile.blade.php
@@ -12,29 +12,36 @@
     <div class="row">
 
         <div class="col-sm-8 col-sm-offset-2">
-            <h2>Zbirni pregled vseh objav</h2>
-            <table class="table table-bordered table-responsive table-striped">
-                <thead>
-                <tr class="glava">
-                    <th># Id</th>
-                    <th>Naslov</th>
-                    <th>Datum objave</th>
-                    <th>Napisano</th>
-                    <th>Zadnja sprmemeba</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($posts as $post)
-                    <tr>
-                        <td>{{ $post->id }}</td>
-                        <td><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a></td>
-                        <td>{{$post->date_published}}</td>
-                        <td>{{ $post->created_at->format('d.M.Y H:i:s') }}</td>
-                        <td>{{ $post->updated_at->diffForHumans() }}</td>
+            @if(count($posts) === 0)
+                <div class="alert alert-info">
+                    <span class="glyphicon glyphicon-info-sign"></span>
+                    Nisi še napisal nobene objave - There is no post created by this user
+                </div>
+            @else
+                <h2>Zbirni pregled vseh objav</h2>
+                <table class="table table-bordered table-responsive table-striped">
+                    <thead>
+                    <tr class="glava">
+                        <th># Id</th>
+                        <th>Naslov</th>
+                        <th>Datum objave</th>
+                        <th>Napisano</th>
+                        <th>Zadnja sprmemeba</th>
                     </tr>
-                @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    @foreach($posts as $post)
+                        <tr>
+                            <td>{{ $post->id }}</td>
+                            <td><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a></td>
+                            <td>{{$post->date_published}}</td>
+                            <td>{{ $post->created_at->format('d.M.Y H:i:s') }}</td>
+                            <td>{{ $post->updated_at->diffForHumans() }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @endif
         </div>
     </div>
 
